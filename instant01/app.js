2d9c84dafc78ae87ca4d4da0093cfb1d8cca242f
phucvin <phucvin@gmail.com>
2025-10-29 13:30:55 -0700

    add todo

--- instant01/app.js
+++ instant01/app.js
@@ -1,52 +1,40 @@
-import { init, i } from "@instantdb/admin";
+import { init, id } from "@instantdb/admin";
 
 import { h, text } from './../shared01.js';
 import * as Todo from './todo.js';
 
 // InstantDB
 const APP_ID = "1ca21621-df0d-4e36-a063-d6a230b491fe";
-const schema = i.schema({
-    entities: {
-        todos: i.entity({
-            text: i.string(),
-            done: i.boolean(),
-            createdAt: i.date(),
-        }),
-    },
-});
-const _db = init({
+const db = init({
     appId: APP_ID,
     adminToken: process.env.INSTANT_APP_ADMIN_TOKEN,
-    schema,
 });
 
 export function initState(context) {
-    context.db = _db;
     return {};
 }
 
 export async function render(state, action, context) {
     const ADD_TODO = 'ADD_TODO';
-    const db = context.db;
 
     switch (action?.t) {
         case ADD_TODO:
-            console.log(ADD_TODO, action);
-            await db.transact([db.tx.todos[id()].update({ title: 'First one' })]);
-            console.log(ADD_TODO, 'done');
+            await db.transact([db.tx.todos[id()].create({
+                text: 'First one',
+                done: false,
+                createdAt: Date.now(),
+            })]);
             break;
     }
 
     let todos = [];
     for (const todo of (await db.query({ todos: {} })).todos) {
-        console.log(todo);
         const props = {
-            iid: todo.id(),
-            text: todo.text(),
+            iid: todo.id,
+            text: todo.text,
         };
         todos.push(...await Todo.render({}, action, context, props));
     }
-    console.log('todos', todos);
 
     return [
         h('head', {}, [
--- instant01/todo.js
+++ instant01/todo.js
@@ -6,6 +6,8 @@ export function initState() {
 
 export async function render(state, action, context, /*props*/ { iid, text: propsText }) {
     return [
-        h('span', { class: 'text-5xl font-semibold text-gray-800"' }, [text(propsText)]),
+        h('div', { class: 'bg-white p-8 rounded-lg shadow-md flex flex-col items-center space-x-4 space-y-4' }, [
+            h('span', { class: 'text-3xl font-semibold text-gray-800"' }, [text(propsText)]),
+        ]),
     ];
 }
