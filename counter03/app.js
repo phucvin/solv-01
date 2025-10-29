7bceda0431f852831c1d2739c474a1f82dd8c669
phucvin <phucvin@gmail.com>
2025-10-28 22:36:11 -0700

    add counter

--- counter03/app.js
+++ counter03/app.js
@@ -4,13 +4,21 @@ import * as counter from './counter.js';
 export function initState(context) {
     return {
         counters: [
-            { state: counter.initState({ startCount: 10}), iid: context.nextIid() },
-            { state: counter.initState({ startCount: 20}), iid: context.nextIid() },
+            { state: counter.initState({ startCount: 10 }), iid: context.nextIid() },
+            { state: counter.initState({ startCount: 20 }), iid: context.nextIid() },
         ],
     };
 }
 
 export async function render(state, action, context) {
+    const ADD_COUNTER = 'ADD_COUNTER';
+
+    switch (action?.t) {
+        case ADD_COUNTER:
+            state.counters.push({ state: counter.initState(), iid: context.nextIid() });
+            break;
+    }
+
     let childCounters = [];
     for (const counterInfo of state.counters) {
         childCounters.push(...await counter.render(counterInfo.state, action, context, { iid: counterInfo.iid }));
@@ -21,6 +29,13 @@ export async function render(state, action, context) {
             h('title', {}, [text('Counter 03 - Solv Prototype')]),
             h('script', { src: "https://cdn.tailwindcss.com" }, [text('')]),
         ]),
-        h('body', { class: "flex flex-col space-y-4 items-center justify-center min-h-screen bg-gray-100" }, childCounters),
+        h('body', { class: "flex flex-col space-y-4 items-center justify-center min-h-screen bg-gray-100" }, [
+            h('div', { class: "space-y-4", }, childCounters),
+            h('button', {
+                class:
+                    'bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded-full text-2xl',
+                onclick: { t: ADD_COUNTER },
+            }, [text('Add Counter')]),
+        ]),
     ];
 }
